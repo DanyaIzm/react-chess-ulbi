8132e38b29c63f2f12e42931db42ef0756be6d35
Daniel <danizmd8@gmail.com>
2022-06-08 16:27:02 +0300

    timer and player panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import BoardComponent from './components/BoardComponent';
+import LostFigures from './components/LostFigures';
+import Timer from './components/Timer';
 import { Board } from './models/Board';
 import { Colors } from './models/Colors';
 import { Player } from './models/Player';
@@ -30,12 +32,26 @@ function App() {
 
   return (
     <div className='app'>
+        <Timer 
+          currentPlayer={currentPlayer}
+          restart={restart}
+        />
         <BoardComponent
           board={board}
           setBoard={setBoard}
           currentPlayer={currentPlayer}
           swapPlayer={swapPlayer}
         />
+        <div>
+          <LostFigures 
+            title={'Чёрные фигуры'}
+            figures={board.lostBlackFigures}
+          />
+          <LostFigures 
+            title={'Белые фигуры'}
+            figures={board.lostWhiteFigures}
+          />
+        </div>
     </div>
   );
 }
