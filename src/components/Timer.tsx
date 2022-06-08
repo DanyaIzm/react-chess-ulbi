c56a691e3ca81ed978ffe758a5acc54401fbf505
Daniel <danizmd8@gmail.com>
2022-06-08 16:56:18 +0300

    Final part of the tutorial

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -8,13 +8,37 @@ interface TimerProps {
 }
 
 const Timer: FC<TimerProps> = ({currentPlayer, restart}) => {
-    const [blackTime, setBlackTime] = useState(300);
-    const [whiteTime, setWhiteTime] = useState(300);
+    const [blackTime, setBlackTime] = useState(3);
+    const [whiteTime, setWhiteTime] = useState(3);
+
+    const [blackLost, setBlackLost] = useState(false);
+    const [whiteLost, setWhiteLost] = useState(false);
+
     const timer = useRef<null | ReturnType<typeof setInterval>>(null);
 
     useEffect(() => {
         startTimer();
     }, [currentPlayer])
+
+    useEffect(() => {
+        if (blackTime === 0) {
+            currentPlayer = null;
+            setBlackLost(true);
+
+            if (timer.current) {
+                clearInterval(timer.current);
+            }
+        }
+        
+        if (whiteTime === 0) {
+            currentPlayer = null;
+            setWhiteLost(true);
+
+            if (timer.current) {
+                clearInterval(timer.current);
+            }
+        }
+    }, [whiteTime, blackTime])
 
     function startTimer() {
         if(timer.current) {
@@ -25,7 +49,9 @@ const Timer: FC<TimerProps> = ({currentPlayer, restart}) => {
             ? decrementWhiteTimer
             : decrementBlackTimer;
 
-        timer.current = setInterval(callback, 1000);
+        if (!blackLost || !whiteLost) {
+            timer.current = setInterval(callback, 1000);
+        }
     }
 
     function decrementBlackTimer() {
@@ -39,6 +65,8 @@ const Timer: FC<TimerProps> = ({currentPlayer, restart}) => {
     const handleRestart = () => {
         setWhiteTime(300);
         setBlackTime(300);
+        setBlackLost(false);
+        setWhiteLost(false);
         restart();
     }
 
@@ -47,8 +75,8 @@ const Timer: FC<TimerProps> = ({currentPlayer, restart}) => {
             <div>
                 <button onClick={handleRestart}>Перезапустить</button>
             </div>
-            <h2>Чёрные - {blackTime}</h2>
-            <h2>Белые - {whiteTime}</h2>
+            <h2 style={{color: blackLost ? 'red' : 'white'}}>Чёрные - {blackTime}</h2>
+            <h2 style={{color: whiteLost ? 'red' : 'white'}}>Белые - {whiteTime}</h2>
         </div>
     )
 }
